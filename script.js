0f983a4fdbef2a375fc52ea4df7955271c5ddad7
Uzedzhi <80003297+Uzedzhi@users.noreply.github.com>
2025-07-19 13:37:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ console.log('Wow its javascript');
 document.querySelector("h1").addEventListener("click", () => {
     alert("Hi");
 });
+document.addEventListener('touchmove', function(e) {
+  e.preventDefault();
+}, { passive: false });
+
+document.addEventListener('wheel', function(e) {
+  e.preventDefault();
+}, { passive: false });
