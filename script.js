687b6b470614d5e0f822b0663d6d6c295c8e7a8a
Uzedzhi <80003297+Uzedzhi@users.noreply.github.com>
2025-08-07 19:48:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,9 @@ document.addEventListener('wheel', function(e) {
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('service-worker.js')
+    navigator.serviceWorker.register('/work-helper-app/service-worker.js')
       .then(reg => console.log('✅ Service Worker зарегистрирован'))
       .catch(err => console.error('❌ Ошибка:', err));
   });
+
 }
