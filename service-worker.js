325d14543316a17f5e7c11feaa0527e4f0a90dda
Uzedzhi <80003297+Uzedzhi@users.noreply.github.com>
2025-08-07 19:56:23 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
 const CACHE_NAME = 'my-cache-v1';
 const urlsToCache = [
-    'index.html',
-    'styles.css',
-    'Adobe Express - file.png',
-    'Group 29.png',
-    'script.js',
-    'images/Group 12 (2).svg',
-    'work_helper.webmanifest',
-    'images/Group 30.svg'
+    '/work-helper-app/index.html',
+    '/work-helper-app/styles.css',
+    '/work-helper-app/Adobe Express - file.png',
+    '/work-helper-app/Group 29.png',
+    '/work-helper-app/script.js',
+    '/work-helper-app/images/Group 12 (2).svg',
+    '/work-helper-app/work_helper.webmanifest',
+    '/work-helper-app/images/Group 30.svg'
 ];
 self.addEventListener('install', event => {
   event.waitUntil(
