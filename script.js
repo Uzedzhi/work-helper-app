f00d4e45d2a795a496a770f71cdd4ed55d492a79
Uzedzhi <80003297+Uzedzhi@users.noreply.github.com>
2025-08-07 21:32:17 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -14,7 +14,15 @@ navigator.serviceWorker.register(swPath).then((registration) => {
   console.log('✅ Firebase SW зарегистрирован');
 
   // Инициализация Firebase
-  const firebaseConfig = { /* твои данные */ };
+  const firebaseConfig = {
+    apiKey: "AIzaSyDI8ufxr-uyh2BEEM3CqtxivtGtW6yONe0",
+    authDomain: "work-helper-app.firebaseapp.com",
+    projectId: "work-helper-app",
+    storageBucket: "work-helper-app.firebasestorage.app",
+    messagingSenderId: "17581970290",
+    appId: "1:17581970290:web:aea03338ced9c76c6743eb",
+    measurementId: "G-1F10L84NKD"
+  };
   firebase.initializeApp(firebaseConfig);
   const messaging = firebase.messaging();
 
