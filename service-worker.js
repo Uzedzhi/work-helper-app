62fbe0bfccfce6f3f01e8dd08c008438427e7405
Uzedzhi <80003297+Uzedzhi@users.noreply.github.com>
2025-08-08 13:10:42 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -22,24 +22,3 @@ self.addEventListener('fetch', event => {
       .then(response => response || fetch(event.request))
   );
 });
-
-
-// ===== Добавляем обработчики Push =====
-self.addEventListener('push', event => {
-  const data = event.data.json();
-  const options = {
-    body: data.body,
-    icon: 'icons/Icon-App-60x60@3x.png',
-    data: { url: data.url }
-  };
-  event.waitUntil(
-    self.registration.showNotification(data.title, options)
-  );
-});
-
-self.addEventListener('notificationclick', event => {
-  event.notification.close();
-  event.waitUntil(
-    clients.openWindow(event.notification.data.url)
-  );
-});
